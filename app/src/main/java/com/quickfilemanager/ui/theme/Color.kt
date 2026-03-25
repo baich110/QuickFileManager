c864c1800f11c020f8a21d18814c533a5fbbac2c
baich110 <154077481+baich110@users.noreply.github.com>
2026-03-25 16:41:14 +0800

    Add Color.kt

---
+++ app/src/main/java/com/quickfilemanager/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.quickfilemanager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// 文件类型颜色
+val FolderColor = Color(0xFFFFA726)      // 橙色 - 文件夹
+val ImageColor = Color(0xFF42A5F5)        // 蓝色 - 图片
+val VideoColor = Color(0xFFAB47BC)        // 紫色 - 视频
+val AudioColor = Color(0xFF66BB6A)        // 绿色 - 音频
+val DocumentColor = Color(0xFF26C6DA)     // 青色 - 文档
+val ArchiveColor = Color(0xFFFFCA28)      // 黄色 - 压缩包
+val UnknownColor = Color(0xFF78909C)      // 灰蓝色 - 未知类型
+
+// Material 3 颜色主题
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
